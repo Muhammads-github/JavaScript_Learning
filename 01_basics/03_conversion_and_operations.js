b776f740948f4990b20a484b256068da0350c5d2
student <sojibjoy923@gmail.com>
2025-02-10 15:53:45 +0600

    Type conversion

---
+++ 01_basics/03_conversion_and_operations.js
@@ -0,0 +1,28 @@
+let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not in Numbers)
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 =>false
+// "" => false
+// "Rock" => true
+
+let someNumber = 33;
+let srtingNumber = String(someNumber);
+// console.log(srtingNumber);
+// console.log(typeof srtingNumber);
+
+// ************Operations***********
