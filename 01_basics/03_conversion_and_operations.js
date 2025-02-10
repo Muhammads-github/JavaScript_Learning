0c44ce2483688741b867d1381ff53bcb55e51435
student <sojibjoy923@gmail.com>
2025-02-10 16:06:33 +0600

    operations

--- 01_basics/03_conversion_and_operations.js
+++ 01_basics/03_conversion_and_operations.js
@@ -26,3 +26,29 @@ let srtingNumber = String(someNumber);
 // console.log(typeof srtingNumber);
 
 // ************Operations***********
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2); // 4
+// console.log(2 - 2); // 0
+// console.log(2 * 2); // 4
+// console.log(2 ** 3); // 8
+// console.log(2 / 2); // 1
+// console.log(2 % 2); // 0
+
+let str1 = "Rock";
+let str2 = " Dimzy";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); // 32
+
+console.log(+true); // 1
+console.log(true); // true
+console.log(+""); // 0
